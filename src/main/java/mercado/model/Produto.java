2a3ca5481070d4e29ec658c2afc2bdef81741c44
MATHEUS E. L. ELEOTERIO <matheuseleoterio90@gmail.com>
2025-10-15 08:08:19 -0300

    feat -- Produto class

--- src/main/java/mercado/model/Produto.java
+++ src/main/java/mercado/model/Produto.java
@@ -1,4 +1,51 @@
 package mercado.model;
 
+import java.math.BigDecimal;
+
 public class Produto {
+    private String nome;
+    private BigDecimal preco;
+    private int quantidade;
+    private String codigoProduto;
+
+    public Produto(String codigoProduto, String nome, BigDecimal preco, int quantidade) {
+        this.codigoProduto = codigoProduto;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getCodigoProduto() {
+        return codigoProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setCodigoProduto(String codigoProduto) {
+        this.codigoProduto = codigoProduto;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+
 }
