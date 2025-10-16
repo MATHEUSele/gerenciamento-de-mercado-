b173a8870f362a71398ab7fa53f9dac476067e4c
MATHEUS E. L. ELEOTERIO <matheuseleoterio90@gmail.com>
2025-10-16 07:49:17 -0300

    feat -- class cliente

--- src/main/java/mercado/model/Cliente.java
+++ src/main/java/mercado/model/Cliente.java
@@ -1,4 +1,33 @@
 package mercado.model;
 
 public class Cliente {
+    private String nome;
+    private int cpf;
+    private String email;
+
+    public Cliente(int cpf, String email, String nome) {
+        this.cpf = cpf;
+        this.email = email;
+        this.nome = nome;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
